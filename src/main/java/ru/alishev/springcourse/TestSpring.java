a20ee89388004ad3b68bc5b149cf5f97d0043151
Dmitry Shishkin <4144442@bk.ru>
2022-04-08 11:49:27 +0300

    Application Context

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -7,8 +7,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        TestBean testBean = context.getBean("testBean",TestBean.class);
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
         context.close();
-        System.out.println(testBean.getName());
     }
 }
