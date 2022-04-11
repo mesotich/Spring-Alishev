613efc33f6be178ec833ddf54c49bd033c2d9f3e
Dmitry Shishkin <4144442@bk.ru>
2022-04-11 19:39:59 +0300

    TestSpring configuration

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -1,18 +1,14 @@
 package ru.alishev.springcourse;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
-        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
-        //ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
-        //System.out.println(classicalMusic1 == classicalMusic2);
         context.close();
     }
 }
